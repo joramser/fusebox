dcd77811f522d86ec345794f338138f06019e8e3
Jorge Ramírez Serrato <jramirezserrato@gmail.com>
2025-10-19 22:31:24 +0300

    Remove not needed HTTP error handling

--- apps/api/src/services/commands.service.ts
+++ apps/api/src/services/commands.service.ts
@@ -1,15 +1,7 @@
 import { spawn } from "node:child_process";
-import { NoProcessFoundError, processesOrchestrator } from "@api/core/processes-orchestrator";
-import { HTTPException } from "hono/http-exception";
+import { processesOrchestrator } from "@api/core/processes-orchestrator";
 
 export const executeCwdCommand = (processName: string, command: string) => {
-  try {
-    const process = processesOrchestrator.get(processName);
-    spawn(command, [process.cwd], { detached: true }).unref();
-  } catch (error) {
-    if (error instanceof NoProcessFoundError) {
-      throw new HTTPException(404, { message: error.message });
-    }
-    throw error;
-  }
+  const process = processesOrchestrator.get(processName);
+  spawn(command, [process.cwd], { detached: true }).unref();
 };
--- apps/api/src/services/process.service.ts
+++ apps/api/src/services/process.service.ts
@@ -1,57 +1,22 @@
-import {
-  NoProcessFoundError,
-  ProcessAlreadyRunningError,
-  processesOrchestrator,
-} from "@api/core/processes-orchestrator";
+import { processesOrchestrator } from "@api/core/processes-orchestrator";
 import { processSocketListenerManager } from "@api/socket/process-socket-listener-manager";
 
-import { HTTPException } from "hono/http-exception";
-
 export const startProcess = (name: string) => {
-  try {
-    const process = processesOrchestrator.get(name);
+  const process = processesOrchestrator.get(name);
 
-    if (process.spawn.status === "init") {
-      processSocketListenerManager.registerProcessSpawn(process.spawn);
-    }
-
-    processesOrchestrator.start(process);
-  } catch (error) {
-    if (error instanceof ProcessAlreadyRunningError) {
-      throw new HTTPException(409, { message: error.message });
-    }
-
-    if (error instanceof NoProcessFoundError) {
-      throw new HTTPException(404, { message: error.message });
-    }
-
-    throw error;
+  if (process.spawn.status === "init") {
+    processSocketListenerManager.registerProcessSpawn(process.spawn);
   }
+
+  processesOrchestrator.start(process);
 };
 
 export const stopProcess = (name: string) => {
-  try {
-    const process = processesOrchestrator.get(name);
-
-    processesOrchestrator.stop(process);
-  } catch (error) {
-    if (error instanceof NoProcessFoundError) {
-      throw new HTTPException(404, { message: error.message });
-    }
-
-    throw error;
-  }
+  const process = processesOrchestrator.get(name);
+  processesOrchestrator.stop(process);
 };
 
 export const clearProcessOutput = (name: string) => {
-  try {
-    const process = processesOrchestrator.get(name);
-    process.spawn.clearOutput();
-  } catch (error) {
-    if (error instanceof NoProcessFoundError) {
-      throw new HTTPException(404, { message: error.message });
-    }
-
-    throw error;
-  }
+  const process = processesOrchestrator.get(name);
+  process.spawn.clearOutput();
 };
